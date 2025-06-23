a36cc3884b452f3e5603c71789031d8680d0be46
Axel Mendoza <axel.mendoza@epita.fr>
2025-06-23 15:04:16 +0200

    add(tests): logs

--- tests/staging_foobar_stack_test.go
+++ tests/staging_foobar_stack_test.go
@@ -8,30 +8,38 @@ import (
 )
 
 func TestFooBarLocalStack(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-    stackDir := "../live/staging/foo-bar"
+	stackDir := "../live/staging/foo-bar"
 
-    // Ensure destroy runs at the end, even if the test fails
-    t.Cleanup(func() {
-        cmdDestroy := exec.Command("terragrunt", "--non-interactive", "stack", "run", "destroy", "--no-stack-generate")
-        cmdDestroy.Dir = stackDir
-        out, err := cmdDestroy.CombinedOutput()
-        require.NoError(t, err, "stack run destroy failed: %s", string(out))
-    })
+	t.Logf("Running test in stackDir: %s", stackDir)
 
-    // Generate
-    cmdGenerate := exec.Command("terragrunt", "stack", "generate")
-    cmdGenerate.Dir = stackDir
-    out, err := cmdGenerate.CombinedOutput()
-    require.NoError(t, err, "stack generate failed: %s", string(out))
+	// Ensure destroy runs at the end, even if the test fails
+	t.Cleanup(func() {
+		t.Log("Running terragrunt destroy as cleanup...")
+		cmdDestroy := exec.Command("terragrunt", "--non-interactive", "stack", "run", "destroy", "--no-stack-generate")
+		cmdDestroy.Dir = stackDir
+		out, err := cmdDestroy.CombinedOutput()
+		t.Logf("Destroy output:\n%s", out)
+		require.NoError(t, err, "stack run destroy failed: %s", string(out))
+	})
 
-    // Apply
-    cmdApply := exec.Command("terragrunt", "--non-interactive", "stack", "run", "apply", "--no-stack-generate")
-    cmdApply.Dir = stackDir
-    out, err = cmdApply.CombinedOutput()
-    require.NoError(t, err, "stack run apply failed: %s", string(out))
+	// Generate
+	t.Log("Running terragrunt stack generate...")
+	cmdGenerate := exec.Command("terragrunt", "stack", "generate")
+	cmdGenerate.Dir = stackDir
+	out, err := cmdGenerate.CombinedOutput()
+	t.Logf("Generate output:\n%s", out)
+	require.NoError(t, err, "stack generate failed: %s", string(out))
 
-    require.Contains(t, string(out), `content = "Foo content: Hello from foo in live repo! (staging) (staging)"`)
+	// Apply
+	t.Log("Running terragrunt stack run apply...")
+	cmdApply := exec.Command("terragrunt", "--non-interactive", "stack", "run", "apply", "--no-stack-generate")
+	cmdApply.Dir = stackDir
+	out, err = cmdApply.CombinedOutput()
+	t.Logf("Apply output:\n%s", out)
+	require.NoError(t, err, "stack run apply failed: %s", string(out))
 
+	t.Log("Checking that output contains expected content...")
+	require.Contains(t, string(out), `content = "Foo content: Hello from foo in live repo! (staging) (staging)"`)
 }
